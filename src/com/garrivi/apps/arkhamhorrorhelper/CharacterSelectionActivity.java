ed309948f993f0fd9d8b2f79aa69dc8c54c97860
Toujimon <gonzalo.arrivi@gmail.com>
2014-05-12 18:00:24 -0400

    Investigator Selector. Not working.
    The list of investigators seems to load fine from the JSON, but it isn't
    shown on the list fragment.

--- src/com/garrivi/apps/arkhamhorrorhelper/CharacterSelectionActivity.java
+++ src/com/garrivi/apps/arkhamhorrorhelper/CharacterSelectionActivity.java
@@ -1,15 +1,26 @@
 package com.garrivi.apps.arkhamhorrorhelper;
 
+import com.garrivi.apps.arkhamhorrorhelper.fragments.CharacterSelectionFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class CharacterSelectionActivity extends Activity {
+public class CharacterSelectionActivity extends FragmentActivity {
+
+	private static final String TAG_FRAGMENT_CHARACTER_SELECTION = "FRAGMENT_CHARACTER_SELECTION";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_character_selection);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager()
+					.beginTransaction()
+					.add(new CharacterSelectionFragment(),
+							TAG_FRAGMENT_CHARACTER_SELECTION).commit();
+		}
 	}
 
 	@Override
---
+++ src/com/garrivi/apps/arkhamhorrorhelper/common/Investigator.java
@@ -0,0 +1,72 @@
+package com.garrivi.apps.arkhamhorrorhelper.common;
+
+public class Investigator {
+	private int id;
+	private String nombre;
+	private String profesion;
+	private String comienzo;
+	private String cordura;
+	private String resistencia;
+	private int expansion;
+
+	public Investigator() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getProfesion() {
+		return profesion;
+	}
+
+	public void setProfesion(String profesion) {
+		this.profesion = profesion;
+	}
+
+	public String getComienzo() {
+		return comienzo;
+	}
+
+	public void setComienzo(String comienzo) {
+		this.comienzo = comienzo;
+	}
+
+	public String getCordura() {
+		return cordura;
+	}
+
+	public void setCordura(String cordura) {
+		this.cordura = cordura;
+	}
+
+	public String getResistencia() {
+		return resistencia;
+	}
+
+	public void setResistencia(String resistencia) {
+		this.resistencia = resistencia;
+	}
+
+	public int getExpansion() {
+		return expansion;
+	}
+
+	public void setExpansion(int expansion) {
+		this.expansion = expansion;
+	}
+
+}
