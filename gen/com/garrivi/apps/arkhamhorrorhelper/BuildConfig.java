091c4902b0b9f1a1b73b90255e04c89c521d001f
Toujimon <gonzalo.arrivi@gmail.com>
2014-05-12 14:06:03 -0400

    First commit. Clean app.

---
+++ gen/com/garrivi/apps/arkhamhorrorhelper/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.garrivi.apps.arkhamhorrorhelper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/garrivi/apps/arkhamhorrorhelper/CharacterSelectionActivity.java
@@ -0,0 +1,22 @@
+package com.garrivi.apps.arkhamhorrorhelper;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CharacterSelectionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_character_selection);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.character_selection, menu);
+		return true;
+	}
+
+}
